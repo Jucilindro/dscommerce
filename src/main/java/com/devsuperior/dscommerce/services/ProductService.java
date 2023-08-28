fdd6374cfac8f8c1dac71404ffacc11e4ebc5e60
Wellington <rodrigueswellington3@gmail.com>
2023-08-28 00:09:05 -0300

    DELETE product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -17,15 +17,17 @@ public class ProductService {
 
     @Autowired
     private ProductRepository repository;
+
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
-        return   new ProductDTO(product);
+        return new ProductDTO(product);
     }
+
     @Transactional(readOnly = true)
-    public Page<ProductDTO > findAll(Pageable pageable) {
+    public Page<ProductDTO> findAll(Pageable pageable) {
         Page<Product> result = repository.findAll(pageable);
-        return  result.map(x -> new ProductDTO(x));
+        return result.map(x -> new ProductDTO(x));
     }
 
     @Transactional
@@ -37,11 +39,16 @@ public class ProductService {
     }
 
     @Transactional
-    public ProductDTO update (Long id, ProductDTO dto) {
+    public ProductDTO update(Long id, ProductDTO dto) {
         Product entity = repository.getReferenceById(id);
         copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        repository.deleteById(id);
     }
 
     private void copyDtoToEntity(ProductDTO dto, Product entity) {
