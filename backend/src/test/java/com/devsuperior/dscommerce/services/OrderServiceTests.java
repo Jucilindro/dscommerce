9affea9c25980a306de8fa0dd085a62d65692a90
wellington-workstation <rodrigueswellington3@gmail.com>
2025-01-29 17:43:36 -0300

    teste no OrderService no metodo insert

--- backend/src/test/java/com/devsuperior/dscommerce/services/OrderServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscommerce/services/OrderServiceTests.java
@@ -2,12 +2,17 @@ package com.devsuperior.dscommerce.services;
 
 import com.devsuperior.dscommerce.dto.OrderDTO;
 import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.repositories.OrderItemRepository;
 import com.devsuperior.dscommerce.repositories.OrderRepository;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.ForbiddenException;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscommerce.tests.OrderFactory;
+import com.devsuperior.dscommerce.tests.ProductFactory;
 import com.devsuperior.dscommerce.tests.UserFactory;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +22,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -33,15 +39,29 @@ public class OrderServiceTests {
     @Mock
     private AuthService authService;
 
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private OrderItemRepository orderItemRepository;
+
+    @Mock
+    private UserService userService;
+
     private Long existingOrderId, nonExistingOrderId;
+    private Long existingProductId, nonExistingProductId;
     private Order order;
     private OrderDTO orderDTO;
     private User admin, client;
+    private Product product;
 
     @BeforeEach
     void setUp() throws Exception {
         existingOrderId = 1L;
         nonExistingOrderId = 2L;
+
+        existingProductId = 1L;
+        nonExistingProductId = 2L;
 
         admin = UserFactory.createCustomAdminUser(1L, "Jef");
         client = UserFactory.createCustomClientUser(2L, "Bob");
@@ -49,8 +69,17 @@ public class OrderServiceTests {
         order = OrderFactory.createOrder(client);
         orderDTO = new OrderDTO(order);
 
+        product = ProductFactory.createProduct();
+
         Mockito.when(repository.findById(existingOrderId)).thenReturn(Optional.of(order));
         Mockito.when(repository.findById(nonExistingOrderId)).thenReturn(Optional.empty());
+
+        Mockito.when(productRepository.getReferenceById(existingProductId)).thenReturn(product);
+        Mockito.when(productRepository.getReferenceById(nonExistingProductId)).thenThrow(EntityNotFoundException.class);
+
+        Mockito.when(repository.save(any())).thenReturn(order);
+
+        Mockito.when(orderItemRepository.saveAll(any())).thenReturn(new ArrayList<>(order.getItems()));
     }
 
     @Test
@@ -90,4 +119,28 @@ public class OrderServiceTests {
 
         Assertions.assertThrows(ResourceNotFoundException.class, () -> service.findById(nonExistingOrderId));
     }
+
+    @Test
+    public void insertShouldReturnOrderDTOWhenAdminLogged() {
+
+        Mockito.when(userService.authenticated()).thenReturn(admin);
+
+        OrderDTO result = service.insert(orderDTO);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(orderDTO.getId(), result.getId());
+    }
+
+    @Test
+    public void insertShouldReturnOrderDTOWhenClientLogged() {
+
+        Mockito.when(userService.authenticated()).thenReturn(client);
+
+        OrderDTO result = service.insert(orderDTO);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(orderDTO.getId(), result.getId());
+    }
+
+
 }
--- backend/src/test/java/com/devsuperior/dscommerce/tests/OrderFactory.java
+++ backend/src/test/java/com/devsuperior/dscommerce/tests/OrderFactory.java
@@ -13,4 +13,5 @@ public class OrderFactory {
         order.getItems().add(orderItem);
         return order;
     }
+
 }
